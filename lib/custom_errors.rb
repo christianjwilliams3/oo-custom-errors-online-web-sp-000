e8075ae093b5a578bdf089f00f79140d7448bd24
christian williams <christian.williamsj@gmail.com>
2019-09-25 06:26:15 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,20 +1,21 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    if person.class != P
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
-  
+ 
   class PartnerError < StandardError
-    
-end
-
+  end 
 end
 
 beyonce = Person.new("Beyonce")
